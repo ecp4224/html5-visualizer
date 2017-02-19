2268516a0721934be90509e080bc51ca16c1b987
Eddie Penta <ecp4224@gmail.com>
2017-02-19 00:24:46 -0500

    Added a clock..because why not

--- js/magic.js
+++ js/magic.js
@@ -38,6 +38,7 @@ window.onload = function() {
   gameWidth = $(window).width();
   gameHeight = $(window).height();
   
+  showName();
   createBars();
   audio.play();
   
@@ -88,7 +89,27 @@ function renderFrame() {
         bar.css({'height': value, 'top': y, 'left': x});
     }
     
+    updateTime();
+    
     requestAnimationFrame(renderFrame);
+}
+
+function updateTime() {
+    var time = $('#time');
+    
+    var now = new Date();
+    
+    var hours = now.getHours();
+    if (hours < 10) {
+        hours = '0' + hours;
+    }
+    
+    var minutes = now.getMinutes();
+    if (minutes < 10) {
+        minutes = '0' + minutes;
+    }
+    
+    time.text(hours + ':' + minutes);
 }
 
 function showAuthor() {
@@ -143,5 +164,36 @@ function showGithub() {
   
   setTimeout(function() {
     tooltip.hide();
+    hideName();
+    bindHover();
   }, 4500);
+}
+
+function bindHover() {
+  $('.bars').on('mouseenter', function() {
+    showName();
+  });
+  
+  $('.bars').on('mouseleave', function() {
+    hideName();
+  });
+}
+
+function hideName() {
+  var name = $('#name');
+  var time = $('#time');
+  name.addClass('down');
+  time.addClass('center');
+  name.removeClass('center');
+  time.removeClass('up');
+}
+
+function showName() {
+  var time = $('#time');
+  var name = $('#name');
+  name.removeClass('up'); //Just in case?
+  time.addClass('up');
+  name.addClass('center');
+  time.removeClass('center');
+  name.removeClass('down');
 }
